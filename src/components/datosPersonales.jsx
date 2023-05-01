e081e218c304278189c0defe9bdc279e99b3bc26
facucam <facundocamilotto@gmail.com>
2023-05-01 19:29:40 -0300

    datosPersonales v1

--- src/components/datosPersonales.jsx
+++ src/components/datosPersonales.jsx
@@ -1,13 +1,18 @@
 function datosPersonales() {
-  return(
-      <datosPersonales>
-        <h2> Facundo Camilotto </h2> //Nombre
-        <h2> 23 años </h2> //edad
-        <h2> facundo@gmail.com </h2> //email
-        <h2> Caballito </h2> //localidad
-        <h2> 11-22334455 </h2> //telefono
+    return(
+        <datosPersonales>
+            <div class='titulo'>
+                <h1> Datos Personales </h1>
+            </div>
+            <div class='contenido'>
+                <h2> Facundo Camilotto </h2> //Nombre
+                <h2> 23 años </h2> //edad
+                <h2> facundo@gmail.com </h2> //email
+                <h2> Caballito </h2> //localidad
+                <h2> 11-22334455 </h2> //telefono
+            </div>
 
-      </datosPersonales>
+        </datosPersonales>
   );
 }
 
